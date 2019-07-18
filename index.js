15b2fdef95946d07f82663fc5eac9f06671a9a90
Mae Rine <mae.s.rine@gmail.com>
2019-07-18 00:25:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = "I don't do much."
 
 var dodger = document.getElementById('dodger')
 
+function addListen(){}
 document.addEventListener('keydown', function(e) {
   console.log(e.which)
 })
