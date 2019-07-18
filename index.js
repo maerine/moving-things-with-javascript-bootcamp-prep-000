47fb7bc94cc8e88b403149b83d7904bcd670ebce
Mae Rine <mae.s.rine@gmail.com>
2019-07-18 00:31:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ function moveDodgerLeft() {
   }
 }
 
+function moveDodgerLeft() {
+  var rightNumbers = dodger.style.right.replace('px', '');
+  var right = parseInt(rightNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.right = `${right - 1}px`;
+  }
+}
+
 $(document).ready(function(){
 
   document.addEventListener('keydown', function(e) {
