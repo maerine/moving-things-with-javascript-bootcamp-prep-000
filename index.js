6c366c62aa456e3d5c6c4cdb1fa5c42f7187cc55
Mae Rine <mae.s.rine@gmail.com>
2019-07-18 00:26:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const app = "I don't do much."
 
 var dodger = document.getElementById('dodger')
 
-function addListen(){
-  document.addEventListener('keydown', function(e) {
-    console.log(e.which);
-  })
-}  
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
 
 $(document).ready(function(){
 
