090b383e7f03fe4fd5fa80e82c1a3563e23690e9
Mae Rine <mae.s.rine@gmail.com>
2019-07-18 00:24:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,16 +6,8 @@ document.addEventListener('keydown', function(e) {
   console.log(e.which)
 })
 
-function pressIt() {
-  $('#game').on('keydown', function(key){
-    if(key.which == 71){
-      alert('You pressed g');
-      }
-  });
-}
-
 $(document).ready(function(){
 
-  pressIt();
+
 
 });
