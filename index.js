7887136b47a04052d504de0e7a736c1a0d6520e4
Mae Rine <mae.s.rine@gmail.com>
2019-07-18 00:23:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const app = "I don't do much."
 
 var dodger = document.getElementById('dodger')
 
+document.addEventListener('keydown', function(e) {
+  console.log(e.which)
+})
+
 function pressIt() {
   $('#game').on('keydown', function(key){
     if(key.which == 71){
